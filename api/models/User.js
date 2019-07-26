c5ffd9625dbd95408d0d36c8b50e13f56f205dea
web-geek <PremK.Lf@gmail.com>
2019-07-26 13:56:51 +0530

    user model made changes

--- api/models/User.js
+++ api/models/User.js
@@ -27,7 +27,7 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: true,
-        minlength: 8,
+        minlength: 4,
         maxlength: 128
     },
     bio: {
--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 export const axios = Axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'http://localhost:3001/'
     // baseURL: '/'
 })
 
